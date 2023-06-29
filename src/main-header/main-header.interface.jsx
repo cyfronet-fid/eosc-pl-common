92e9bf088c802221ce2b23c74ef131fb5236b8b7
Jarek Zbozień <j.zbozien@gmail.com>
2023-06-29 14:59:16 +0200

    fix: delete beyond logo

--- src/main-header/main-header.interface.jsx
+++ src/main-header/main-header.interface.jsx
@@ -58,7 +58,6 @@ class EoscCommonMainHeader extends Component {
                 <a href="https://eosc.pl/" className="header-logo">
                   &nbsp;
                 </a>
-                <div class="beyond-logo"></div>
               </div>
 
               <input className="menu-btn" type="checkbox" id="menu-btn" />
