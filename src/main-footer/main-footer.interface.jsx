12afada88f48a8405a2e19f7de750abb061591d0
mich.kolomanski <mich.kolomanski@gmail.com>
2025-06-12 16:08:27 +0200

    fix: [#6] update links for privacy policy and terms of use

--- src/main-footer/main-footer.interface.jsx
+++ src/main-footer/main-footer.interface.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Component } from "preact";
 import { environment } from "../../env/env";
-import EoscMainFooterLogoBar from "./main-footer-logo-bar.component";
+// import EoscMainFooterLogoBar from "./main-footer-logo-bar.component";
 import EoscMainFooterCols from "./main-footer-cols.component";
 import { fieldsToCamelCase, usePropTypes } from "../../core/utils";
 import { renderComponent } from "../../core/render";
@@ -14,22 +14,24 @@ class EoscCommonMainFooter extends Component {
   static propTypes = {
     production: PropTypes.bool,
     socialIcons: PropTypes.arrayOf(
-        PropTypes.shape({
-          class: PropTypes.string.isRequired,
-          url: PropTypes.string.isRequired,
-        })
+      PropTypes.shape({
+        class: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+      })
     ),
     termsOfUse: PropTypes.string,
-    privacyPolicy: PropTypes.string
+    privacyPolicy: PropTypes.string,
   };
 
   static defaultProps = {
     production: environment.production,
     socialIcons: environment.mainFooterConfig.socials,
+    termsOfUse: null,
+    privacyPolicy: null,
   };
 
   render(props) {
-    const { production, termsOfUse, privacyPolicy} = fieldsToCamelCase(usePropTypes(props, EoscCommonMainFooter));
+    const { production, termsOfUse, privacyPolicy } = fieldsToCamelCase(usePropTypes(props, EoscCommonMainFooter));
     return (
       <div>
         <footer className={`eosc-common footer pt-3 pb-3 ${production ? "" : "demo"}`}>
@@ -38,9 +40,11 @@ class EoscCommonMainFooter extends Component {
           </div>
         </footer>
         <div className="eosc-common copyright container">
-          <span className="copy-text">Copyright 2025 &nbsp;&nbsp; | &nbsp;&nbsp; All rights reserved</span> &nbsp;&nbsp;
-          | &nbsp;&nbsp;
-          <a href="https://eosc-portal.eu/privacy-policy-summary">Privacy policy</a>
+          <span className="copy-text">Copyright 2025 &nbsp;&nbsp; | &nbsp;&nbsp; All rights reserved</span>
+          &nbsp;&nbsp; | &nbsp;&nbsp;
+          <a href="https://eosc.pl/privacy-policy">Privacy policy</a>
+          &nbsp;&nbsp; | &nbsp;&nbsp;
+          <a href="https://eosc.pl/terms-of-use">Terms of use</a>
         </div>
       </div>
     );
