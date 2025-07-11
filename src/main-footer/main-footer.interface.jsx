4113e9202e8320397b447af219d440f3056b5d3a
Pawel Gorczyca <goreck888@gmail.com>
2025-07-11 11:52:06 +0200

    chore: Update dependencies
    Update dependencies to the newest possible versions

--- src/main-footer/main-footer.interface.jsx
+++ src/main-footer/main-footer.interface.jsx
@@ -2,31 +2,31 @@ import PropTypes from "prop-types";
 import { Component } from "preact";
 import { environment } from "../../env/env";
 import EoscMainFooterCols from "./main-footer-cols.component";
-import { fieldsToCamelCase, usePropTypes } from "../../core/utils";
 import { renderComponent } from "../../core/render";
+import { fieldsToCamelCase, usePropTypes } from "../../core/utils";
 
 /**
  * @version 1.1
  */
-// eslint-disable-next-line no-unused-vars
+
 class EoscCommonMainFooter extends Component {
   static propTypes = {
     production: PropTypes.bool,
     socialIcons: PropTypes.arrayOf(
       PropTypes.shape({
         class: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired
       })
     ),
     termsOfUse: PropTypes.string,
-    privacyPolicy: PropTypes.string,
+    privacyPolicy: PropTypes.string
   };
 
   static defaultProps = {
     production: environment.production,
     socialIcons: environment.mainFooterConfig.socials,
     termsOfUse: null,
-    privacyPolicy: null,
+    privacyPolicy: null
   };
 
   render(props) {
@@ -37,12 +37,12 @@ class EoscCommonMainFooter extends Component {
           <div className="container">
             <EoscMainFooterCols termsOfUse={termsOfUse} privacyPolicy={privacyPolicy} />
           </div>
+          <div className="eosc-common copyright container">
+            <span className="copy-text">Copyright 2025 &nbsp;&nbsp; | &nbsp;&nbsp; All rights reserved</span> &nbsp;&nbsp;
+                | &nbsp;&nbsp;
+            <a href="https://eosc-portal.eu/privacy-policy-summary">Privacy policy</a>
+          </div>
         </footer>
-        <div className="eosc-common copyright container">
-          <span className="copy-text">Copyright 2025 &nbsp;&nbsp; | &nbsp;&nbsp; All rights reserved</span> &nbsp;&nbsp;
-          | &nbsp;&nbsp;
-          <a href="https://eosc-portal.eu/privacy-policy-summary">Privacy policy</a>
-        </div>
       </div>
     );
   }
