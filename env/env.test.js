693eb9a24df5a170e1d5dbead96ba026b0dc645f
Pawel Gorczyca <goreck888@gmail.com>
2025-07-11 11:52:06 +0200

    chore: Update dependencies
    Update dependencies to the newest possible versions

---
+++ env/env.test.js
@@ -0,0 +1,18 @@
+const mainHeaderConfig = require("../configurations/main-header.development.config.json");
+const mainFooterConfig = require("../configurations/main-footer.development.config.json");
+const euInformationConfig = require("../configurations/eu-information.development.json");
+const defaultConfiguration = require("../configurations/configuration.development.json");
+
+const environment = {
+    mainHeaderConfig,
+    mainFooterConfig,
+    defaultConfiguration,
+    euInformationConfig,
+    production: false,
+    windowTagName: "eosccommon",
+};
+exports.environment = environment;
+
+if (!window[environment.windowTagName]) {
+    window[environment.windowTagName] = {};
+}
--- src/main-footer/main-footer-cols.component.jsx
+++ src/main-footer/main-footer-cols.component.jsx
@@ -4,66 +4,64 @@ import { environment } from "../../env/env";
 import { usePropTypes } from "../../core/utils";
 
 export default class EoscMainFooterCols extends Component {
-    static propTypes = {
-        cols: PropTypes.arrayOf(
-            PropTypes.arrayOf(
-                PropTypes.shape({
-                    label: PropTypes.string.isRequired,
-                    url: PropTypes.string,
-                    navBtns: PropTypes.arrayOf(
-                        PropTypes.shape({
-                            label: PropTypes.string.isRequired,
-                            url: PropTypes.string.isRequired,
-                        })
-                    ).isRequired,
-                })
-            )
-        ),
-        termsOfUse: PropTypes.string,
-        privacyPolicy: PropTypes.string
-    };
+  static propTypes = {
+    cols: PropTypes.arrayOf(
+      PropTypes.arrayOf(
+        PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          url: PropTypes.string,
+          navBtns: PropTypes.arrayOf(
+            PropTypes.shape({
+              label: PropTypes.string.isRequired,
+              url: PropTypes.string.isRequired
+            })
+          ).isRequired
+        })
+      )
+    ),
+    termsOfUse: PropTypes.string,
+    privacyPolicy: PropTypes.string
+  };
 
-    static defaultProps = {
-        cols: environment.mainFooterConfig.cols,
-    };
+  static defaultProps = {
+    cols: environment.mainFooterConfig.cols
+  };
 
-    renderLink(link, props) {
-        const type = link.type ?? 'link';
-        if (type === 'link') {
-            return (<li>{link.url ? <a href={link.url}>{link.label}</a> : link.label}</li>)
-        } else if (type === 'termsOfUse' && props.termsOfUse) {
-            return (<li><a href={props.termsOfUse}>{link.label}</a></li>)
-        } else if (type === 'privacyPolicy' && props.privacyPolicy) {
-            return (<li><a href={props.privacyPolicy}>{link.label}</a></li>)
-        }
-        return null;
+  renderLink(link, props) {
+    const type = link.type ?? "link";
+    if (type === "link") {
+      return (<li>{link.url ? <a href={link.url}>{link.label}</a> : link.label}</li>);
+    } else if (type === "termsOfUse" && props.termsOfUse) {
+      return (<li><a href={props.termsOfUse}>{link.label}</a></li>);
+    } else if (type === "privacyPolicy" && props.privacyPolicy) {
+      return (<li><a href={props.privacyPolicy}>{link.label}</a></li>);
     }
+    return null;
+  }
 
-    render(props) {
-        const propsValidated = usePropTypes(props, EoscMainFooterCols);
-        const cols = propsValidated.cols;
-        return (
-          <div className="d-flex mt-5 pb-2">
-              <div className="d-flex mr-4">
-                  <a href="https://eosc.pl/" className="logo-two"></a>
-              </div>
-              {cols.map((col) => (
-              <div className="d-flex">
-                  {col.map((list) => (
-                    <ul>
-                        <li>
-                            <div className="title">{list.url ? <a href={list.url}>{list.label}</a> : list.label}</div>
-                        </li>
-                        {!!list.navBtns && list.navBtns.length > 0 ? (
-                            list.navBtns.map((btn) => this.renderLink(btn, propsValidated))
-                        ) : (
-                            <Fragment />
-                        )}
-                    </ul>
-                        ))}
-              </div>
-                ))}
+  render(props) {
+    const propsValidated = usePropTypes(props, EoscMainFooterCols);
+    const cols = propsValidated.cols;
+    return (
+      <div className="d-flex mt-5 pb-2">
+        <div className="d-flex mr-4">
+          <a href="https://eosc.pl/" className="logo-two"></a>
+        </div>
+        {cols.map((col) => (
+          <div className="d-flex column">
+            <ul>
+              <li>
+                <div className="title">{col.url ? <a href={col.url}>{col.label}</a> : col.label}</div>
+              </li>
+              {!!col.navBtns && col.navBtns.length > 0 ? (
+                col.navBtns.map((btn) => this.renderLink(btn, propsValidated))
+              ) : (
+                <Fragment />
+              )}
+            </ul>
           </div>
-        );
-    }
+        ))}
+      </div>
+    );
+  }
 }
