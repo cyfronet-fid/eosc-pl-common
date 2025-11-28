849127091259c88c40edef44149045b1fc8e0842
Joanna Rogoz <joanna.rogoz33@gmail.com>
2025-11-28 12:34:18 +0100

    fix: remove "o eosc pl" from footer

--- env/env.beta.js
+++ env/env.beta.js
@@ -1,17 +1,15 @@
 const mainHeaderConfig = require("../configurations/main-header.beta.config.json");
 const mainFooterConfig = require("../configurations/main-footer.development.config.json");
-const euInformationConfig = require("../configurations/eu-information.development.json");
 const defaultConfiguration = require("../configurations/configuration.development.json");
 
 const environment = {
   mainHeaderConfig,
   mainFooterConfig,
   defaultConfiguration,
-  euInformationConfig,
   marketplaceUrl: "https://marketplace.eosc.pl",
   dashboardUrl: "https://eosc-user-dashboard.docker-fid.grid.cyf-kr.edu.pl",
   production: false,
-  windowTagName: "eosccommon",
+  windowTagName: "eosccommon"
 };
 exports.environment = environment;
 
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { default as main_header } from "/home/eland/Desktop/eosc-pl-common/src/main-header/main-header.interface.jsx";
 
 import { default as main_footer } from "/home/eland/Desktop/eosc-pl-common/src/main-footer/main-footer.interface.jsx";
-import { default as eu_information } from "/home/eland/Desktop/eosc-pl-common/src/eu-information.interface.jsx";
 
 main_header;
 main_footer;
-eu_information;
