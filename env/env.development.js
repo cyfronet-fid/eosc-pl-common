d004d32fcc36387dae60a2298522578908bb9f6d
szymon figura <szymon.figura@cyfronet.pl>
2026-08-27 16:27:33 +0200

    feat: changes after review

--- env/env.development.js
+++ env/env.development.js
@@ -3,15 +3,70 @@ const mainFooterConfig = require("../configurations/main-footer.development.conf
 const totopWrapperConfig = require("../configurations/totop-wrapper.development.json");
 const defaultConfiguration = require("../configurations/configuration.development.json");
 
-const environment = {
+export const discoveryhubUrl = "https://eosc.pl";
+export const marketplaceUrl = "https://marketplace.eosc.pl";
+export const bosUrl = "https://bos.eosc.pl";
+export const onedataUrl = "https://data.eosc.pl"; // don't remove even if IDE suggest so, it could be used with different config
+
+export const environment = {
   mainHeaderConfig,
   mainFooterConfig,
   defaultConfiguration,
   totopWrapperConfig,
   production: false,
-  windowTagName: "eosccommon"
+  windowTagName: "eosccommon",
+  customUserTabs: [
+    {
+      id: "Provider",
+      name: "Provider",
+      links: [
+        {
+          caption: "Become provider",
+          href: `${discoveryhubUrl}/#become-provider`,
+          dividerAfter: true
+        },
+        {
+          caption: "Documentation",
+          href: `${discoveryhubUrl}/documentation`
+        }
+      ]
+    }
+  ],
+  customProviderTabs: [
+    {
+      id: "provider",
+      name: "Provider",
+      links: [
+        {
+          caption: "Backoffice",
+          href: `${marketplaceUrl}/backoffice/services`
+        },
+        {
+          caption: "Ordering system",
+          href: `${bosUrl}`,
+          dividerAfter: true
+        },
+        {
+          caption: "+ Add new service",
+          href: `${marketplaceUrl}/backoffice/services/new`
+        },
+        {
+          caption: "+ Add new provider",
+          href: `${marketplaceUrl}/backoffice/providers/new/wizard`
+        },
+        {
+          caption: "+ Add new catalogue",
+          href: `${marketplaceUrl}/backoffice/catalogues/new`,
+          dividerAfter: true
+        },
+        {
+          caption: "Documentation",
+          href: `${discoveryhubUrl}/documentation`
+        }
+      ]
+    }
+  ]
 };
-exports.environment = environment;
 
 if (!window[environment.windowTagName]) {
   window[environment.windowTagName] = {};
--- src/main-header/main-header.interface.spec.jsx
+++ src/main-header/main-header.interface.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/preact";
+import { render, screen, waitFor } from "@testing-library/preact";
 import { h } from "preact";
 import userEvent from "@testing-library/user-event";
 import Cookies from "js-cookie";
@@ -11,7 +11,7 @@ import "window-resizeto/polyfill";
 import {
   AUTOLOGIN_COOKIE_NAME,
   getCookieConfig,
-  LOGIN_ATTEMPT_COOKIE_NAME,
+  LOGIN_ATTEMPT_COOKIE_NAME
 } from "./auto-login.utils";
 
 jest.mock("react-responsive", () => ({
